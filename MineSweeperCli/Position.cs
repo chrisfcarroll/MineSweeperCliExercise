4e7bffb2030602d6b4db546a67bae1daa6106f28
Chris F Carroll <chris.carroll@unforgettable.me.uk>
2022-05-25 01:04:47 +0100

    make Position a readonly record

--- MineSweeperCli/Position.cs
+++ MineSweeperCli/Position.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 
 namespace MineSweeperCli;
 
-public record struct Position(int X, int Y)
+public readonly record struct Position(int X, int Y)
 {
     public override string ToString() => $"{(char)(64+Y)}{X}";
     public Position Add(int addX, int addY) { return new Position(X + addX, Y + addY); }
